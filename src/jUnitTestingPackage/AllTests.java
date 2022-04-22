bffd4f4f4973e33907f591376dcb5b4d365bb9f7
USER <USER@10.12.22.176>
2022-04-22 22:49:46 +0530

    Testing

--- src/jUnitTestingPackage/AllTests.java
+++ src/jUnitTestingPackage/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({ TestAddNumbers.class,addTwoStrings.class})
 public class AllTests {
 
 }
